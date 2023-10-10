4bdd10621dc15b0b6a336857c0f659c97cf818f4
EmmaFodor <fodor_ema@yahoo.com>
2023-10-10 17:44:58 +0300

    WebSocket Server

---
+++ src/gateway/gateway.ts
@@ -0,0 +1,33 @@
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { OnModuleInit } from '@nestjs/common';
+@WebSocketGateway({
+  cors: {
+    origin: ['http://localhost:3000'],
+  },
+})
+export class MyGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit(): any {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('Connected');
+    });
+  }
+
+  @SubscribeMessage('newMessage')
+  onNewMessage(@MessageBody() body: any) {
+    console.log(body);
+    this.server.emit('onMessage', {
+      message: 'New Message',
+      content: body,
+    });
+  }
+}
